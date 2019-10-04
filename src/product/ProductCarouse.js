ec8fa4fc844a76f3435c3cd645c79fef0cd1455b
Ahmed Aziz <ahmed83@me.com>
2019-10-04 23:53:59 +0200

    change images home page adver

--- src/product/ProductCarouse.js
+++ src/product/ProductCarouse.js
@@ -3,26 +3,27 @@ import { UncontrolledCarousel } from "reactstrap";
 
 const items = [
   {
-    src:
-      "https://scontent-ams4-1.xx.fbcdn.net/v/t1.0-9/67314724_2925392360867250_6633650372483219456_n.jpg?_nc_cat=105&_nc_oc=AQmasGIPGvwH4aQ1Nf9HfP0UD-qPZjip941Dk2SGpEOUxYs7Osp7b14oQAbzWGbmYjTqx-IOq1b9vo03pj9vmhg2&_nc_ht=scontent-ams4-1.xx&oh=c8e8c0bd39ba96c2166ca8a5bc374baa&oe=5DF437D4",
+    src: require("../assets/3d-blue.jpg"),
     // altText: "Slide 1",
-    caption: "Slide 1"
+    caption: ""
     // header: "Slide 1 Header"
   },
   {
-    src:
-      "https://scontent-ams4-1.xx.fbcdn.net/v/t1.0-9/67314724_2925392360867250_6633650372483219456_n.jpg?_nc_cat=105&_nc_oc=AQmasGIPGvwH4aQ1Nf9HfP0UD-qPZjip941Dk2SGpEOUxYs7Osp7b14oQAbzWGbmYjTqx-IOq1b9vo03pj9vmhg2&_nc_ht=scontent-ams4-1.xx&oh=c8e8c0bd39ba96c2166ca8a5bc374baa&oe=5DF437D4",
-    caption: "Slide 2"
+    src: require("../assets/3d-yellow.png"),
+    caption: ""
   },
   {
-    src:
-      "https://scontent-ams4-1.xx.fbcdn.net/v/t1.0-9/67314724_2925392360867250_6633650372483219456_n.jpg?_nc_cat=105&_nc_oc=AQmasGIPGvwH4aQ1Nf9HfP0UD-qPZjip941Dk2SGpEOUxYs7Osp7b14oQAbzWGbmYjTqx-IOq1b9vo03pj9vmhg2&_nc_ht=scontent-ams4-1.xx&oh=c8e8c0bd39ba96c2166ca8a5bc374baa&oe=5DF437D4",
-    caption: "Slide 3"
+    src: require("../assets/3d-blue-yellow.jpg"),
+    caption: ""
   }
 ];
 
 const ProductCarouse = () => (
-  <UncontrolledCarousel captionText={items.caption} key={items.caption} items={items} />
+  <UncontrolledCarousel
+    captionText={items.caption}
+    key={items.src}
+    items={items}
+  />
 );
 
 export default ProductCarouse;
