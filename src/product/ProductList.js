b6582f5cf5e4c102605f8fcbae350f2fe1e6dfa2
Ahmed Aziz <ahmed83@me.com>
2019-10-11 07:24:13 +0200

    add image recouce to the pages

--- src/product/ProductList.js
+++ src/product/ProductList.js
@@ -24,7 +24,7 @@ const ProductList = () => {
                   <CardImg
                     top
                     width="100%"
-                    src="https://scontent-ams4-1.xx.fbcdn.net/v/t1.0-9/71246364_3071676162905535_4585475752870281216_n.jpg?_nc_cat=107&_nc_oc=AQksHZ8A16yGLzYX4B__gCmcK4gx9lJr4UNAIAQRBweJ3CiXjhoRbo2ajPmw5hB4FXgyM7ZWPZlU2n1ErTiR7_xZ&_nc_ht=scontent-ams4-1.xx&oh=33f9131fd5d0d3edae5c10e39591f82f&oe=5E36DAF3"
+                    src={`/upload/${product.imageName}`}
                     alt="Card image cap"
                   />
                 </a>
