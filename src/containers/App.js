437a3228c88b11e74a66a426b326b34af8e423f5
Ahmed Aziz <ahmed83@me.com>
2019-10-12 09:40:45 +0200

    Refactoring: Structure the whole Project 1

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,12 @@ import "./App.css";
 
 import HomePage from "./HomePage";
 import Footer from "./Footer";
-import AdminPage from "admin/AdminPage";
+import AdminPage from "components/admin/AdminPage";
 import LoginPage from "login/LoginPage";
-import ProductDetailsPage from "product/ProductDetails";
+import ProductDetailsPage from "components/product/ProductDetails";
 import { Container } from "reactstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CartPage from "cart/CartPage";
+import CartPage from "components/cart/CartPage";
 
 const App = () => {
   return (
